a16b196b0abf414ffc933ae3bef5692f7f10269d
Victor Morales <victor.morales@intel.com>
2016-04-06 15:50:36 -0500

    Fix ceilometer plugin installation in swift controller script

--- apt/swift-controller.sh
+++ apt/swift-controller.sh
@@ -12,13 +12,11 @@ apt-get install -y swift swift-proxy python-swiftclient \
   python-keystoneclient python-keystonemiddleware \
   memcached
 
-./swift.sh
-
 # Object Storage - Telemetry services
 
-# Workaround for https://bugs.launchpad.net/openstack-manuals/+bug/1462947
-apt-get install -y python-pip
-pip install ceilometermiddleware==0.1.0
+apt-get install -y python-ceilometermiddleware
+
+./swift.sh
 
 ./configure_ceilometer_object_storage_controller.sh
 
