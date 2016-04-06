aef85fbd95afcb7dc27dcb3daca1be5993d0bf0c
Victor Morales <victor.morales@intel.com>
2016-04-06 15:49:09 -0500

    Add execution persmissions to opendaylight script

--- apt/opendaylight.sh
+++ apt/opendaylight.sh
@@ -11,7 +11,6 @@ filename=${folder}.zip
 wget https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/$distribution_name/$release_name/$filename -O /tmp/$filename
 
 unzip /tmp/$filename
-rm /tmp/$filename
 cd $folder/
 # Enable OpenStack features
 sed -i "s/featuresBoot=config,standard,region,package,kar,ssh,management/featuresBoot=config,standard,region,package,kar,ssh,management,odl-base-all,odl-aaa-authn,odl-restconf,odl-nsf-all,odl-adsal-northbound,odl-mdsal-apidocs,odl-ovsdb-openstack,odl-ovsdb-northbound,odl-dlux-core/g" etc/org.apache.karaf.features.cfg
