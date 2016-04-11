cbb17c2788bcd9c61a68774a6f036e3d753432a3
Victor Morales <victor.morales@intel.com>
2016-04-11 08:15:55 -0500

    Add reference for external repo

--- apt/repo.sh
+++ apt/repo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 apt-get install -y ubuntu-cloud-keyring
-cp /root/scripts/sources.list /etc/apt/sources.list
+if [ -f /root/shared/sources.list ]
+then
+  cp /root/scripts/sources.list /etc/apt/sources.list
+fi
 apt-get update -y && apt-get dist-upgrade -y
 
 apt-get install -y crudini python-mysqldb python-openstackclient
