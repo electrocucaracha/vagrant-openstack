0346b4e61fe33969678e781a0ae0e708f87bb0ab
Victor Morales <victor.morales@intel.com>
2016-04-11 22:27:19 -0500

    Add create domain instruction to initial identity setup

--- shared/setup/create_initial_accounts.sh
+++ shared/setup/create_initial_accounts.sh
@@ -3,37 +3,56 @@
 export OS_TOKEN=${token}
 export OS_URL=http://${IDENTITY_HOSTNAME}:35357/v3
 export OS_IDENTITY_API_VERSION=3
-unset OS_PROJECT_DOMAIN_ID OS_IMAGE_API_VERSION OS_USER_DOMAIN_ID \
+unset OS_PROJECT_DOMAIN_NAME OS_IMAGE_API_VERSION OS_USER_DOMAIN_NAME \
   OS_PROJECT_NAME OS_PASSWORD OS_AUTH_URL OS_USERNAME OS_TENANT_NAME
 
-# Create the service entity and API endpoints
-
+# Create the service entity for the Identity service
 openstack service create \
   --name keystone --description "OpenStack Identity" identity
 
+# Create the Identity service API enpoints
 openstack endpoint create --region RegionOne \
-  identity public http://${IDENTITY_HOSTNAME}:5000/v2.0
+  identity public http://${IDENTITY_HOSTNAME}:5000/v3
 
 openstack endpoint create --region RegionOne \
-  identity internal http://${IDENTITY_HOSTNAME}::5000/v2.0
+  identity internal http://${IDENTITY_HOSTNAME}::5000/v3
 
 openstack endpoint create --region RegionOne \
-  identity admin http://${IDENTITY_HOSTNAME}:35357/v2.0
+  identity admin http://${IDENTITY_HOSTNAME}:35357/v3
 
+# Create default domain
+openstack domain create --description "Default Domain" default
+
+# Create the admin project
 openstack project create --domain default \
   --description "Admin Project" admin
+
+# Create the admin user
 openstack user create --domain default \
   --password "${ADMIN_PASS}" admin
+
+# Create the admin role
 openstack role create admin
+
+# Add the admin role tod the admin project and user
 openstack role add --project admin --user admin admin
+
+# Create the servcie project
 openstack project create --domain default \
   --description "Service Project" service
+
+# Create the demo project
 openstack project create --domain default \
   --description "Demo Project" demo
 
+# Create the demo user
 openstack user create --domain default \
   --password "${DEMO_PASS}" demo
+
+# Create the user role
 openstack role create user
+
+# Add the user role to the demo project and user
 openstack role add --project demo --user demo user
 
 unset OS_TOKEN OS_URL
