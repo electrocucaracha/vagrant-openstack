89a97b1d89596092b429c32902b55ccb19b1c2f8
Victor Morales <victor.morales@intel.com>
2016-05-18 14:21:51 -0500

    Update horizon script to mitaka release

--- apt/horizon.sh
+++ apt/horizon.sh
@@ -8,11 +8,31 @@ cd /root/shared/setup
 
 # Dashboard
 
-# 1. Install OpenStack Dashboard and dependencies
+# Install OpenStack Dashboard and dependencies
 apt-get install -y openstack-dashboard
 
-# 2. Configure settings
+# Configure the dashboard to use OpenStack services
 sed -i "s/OPENSTACK_HOST = \"127.0.0.1\"/OPENSTACK_HOST = \"${IDENTITY_HOSTNAME}\"/g" /etc/openstack-dashboard/local_settings.py
 
-# 3. Restart services
+# Enable the Identity API version 3
+sed -i "s/http:\/\/%s:5000\/v2.0/http:\/\/%s:5000\/v3/g" /etc/openstack-dashboard/local_settings.py
+
+# Enable support for domains
+sed -i "s/#OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = False/OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True/g" /etc/openstack-dashboard/local_settings.py
+
+cat <<EOL >> /etc/openstack-dashboard/local_settings.py
+OPENSTACK_API_VERSIONS = {
+    "identity": 3,
+    "image": 2,
+    "volume": 2,
+}
+EOL
+
+# Configure default as the default domain for users that you create via the dashboard
+sed -i "s/#OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'default'/OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'default'/g" /etc/openstack-dashboard/local_settings.py
+
+# Configure user as the default role for users that you create via the dashboard
+sed -i "s/OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"_member_\"/OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"user\"/g" /etc/openstack-dashboard/local_settings.py
+
+# Finalize installation
 service apache2 reload
