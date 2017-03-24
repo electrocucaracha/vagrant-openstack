14f6683f0b5f2e081287353196bd197672f49fcd
Victor Morales <victor.morales@intel.com>
2017-03-23 22:57:28 -0500

    Upgrade Memcached script to Ubuntu Xenial image

--- apt/memcached.sh
+++ apt/memcached.sh
@@ -4,11 +4,14 @@ cd /root/shared
 source configure.sh
 cd setup
 
-apt-get update -y && apt-get dist-upgrade -y
+apt-get update -y
 
 # Memcached service
 
-# 1. Install database server
+# 1. Install memcached service
 apt-get install -y memcached python-memcache
+
+# 2. Configure to listen a specific service
+sed -i "s/-l 127.0.0.1/-l 0.0.0.0/g" /etc/memcached.conf
 
 service memcached restart
