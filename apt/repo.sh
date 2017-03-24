018f2a6beac4ef49896d20e66be4f1ac6854c419
Victor Morales <victor.morales@intel.com>
2017-03-23 22:39:00 -0500

    Upgrade to OpenStack Ocata Release

--- apt/repo.sh
+++ apt/repo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 apt-get install -y software-properties-common python-software-properties
-add-apt-repository -y cloud-archive:mitaka
+add-apt-repository -y cloud-archive:ocata
 if [ -f /root/shared/sources.list ]
 then
   cp /root/scripts/sources.list /etc/apt/sources.list
 fi
-apt-get update -y && apt-get dist-upgrade -y
 
-apt-get install -y crudini python-mysqldb python-openstackclient
+apt-get update -y
+apt-get install -y crudini python-openstackclient
 
 if [ "$CONFIGURATION" !=  "_all-in-one" ]
 then
